38b1660994dd2e9d0d865e0159213bfcc1745339
rita <D1225488@o365.fcu.edu.tw>
2025-06-03 01:50:23 +0800

    1

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ function validateInputs() {
 username.addEventListener('input', validateInputs);
 password.addEventListener('input', validateInputs);
 
-// 當按下登入按鈕時，顯示警告
 loginBtn.addEventListener('click', () => {
     alert('⚠️ 這是釣魚網站，請勿輸入真實帳號密碼！');
 });
